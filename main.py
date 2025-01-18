00e8da1c27e576bf3a8e62f076a331d33d2fc02a
Piotrof <piotrowicz.jakub.e@gmail.com>
2025-01-19 00:56:25 +0100

    Added user display at FastAPI startup

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ if custom_lib_path not in sys.path:
 from fastapi import FastAPI
 import uvicorn
 from endpoints import router
+import getpass
+print("FastAPI is running as:", getpass.getuser())
 
 # Import the custom middleware
 from ApiKeyAuth import ApiKeyAuthMiddleware
